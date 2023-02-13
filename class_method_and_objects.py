b4f1e7badf39ff8a16981ce7e08c3e91660aa77a
Nikhil Shirsath <contact@nikhilshirsath.com>
2023-02-13 13:15:47 +0530

    new py file

---
+++ class_method_and_objects.py
@@ -0,0 +1,24 @@
+# #### Exercise 1:
+#
+# 1. Write a Rectangle class in Python language, allowing you to build a rectangle with length and width attributes.
+# 2. Create a Perimeter() method to calculate the perimeter of the rectangle and a Area() method to calculate the area of the rectangle.
+# 3. Create a method display() that display the length, width, perimeter and area of an object created using an instantiation on rectangle class.
+# 4. Create a Parallelepipede child class inheriting from the Rectangle class and with a height attribute and another Volume() method to calculate the volume of the Parallelepiped.
+# #%% md
+# #### Exercise 2:
+#
+# 1. Create a Python class Person with attributes: name and age of type string.
+# 2. Create a display() method that displays the name and age of an object created via the Person class.
+# 3. Create a child class Student  which inherits from the Person class and which also has a section attribute.
+# 4. Create a method displayStudent() that displays the name, age and section of an object created via the Student class.
+# 5. Create a student object via an instantiation on the Student class and then test the displayStudent method.
+# #%% md
+# #### Exercise 3:
+#
+# 1. Create a Python class called BankAccount which represents a bank account, having as attributes: accountNumber (numeric type), name (name of the account owner as string type), balance.
+# 2. Create a constructor with parameters: accountNumber, name, balance.
+# 3. Create a Deposit() method which manages the deposit actions.
+# 4. Create a Withdrawal() method  which manages withdrawals actions.
+# 5. Create an bankFees() method to apply the bank fees with a percentage of 5% of the balance account.
+# 6. Create a display() method to display account details.
+# 7. Give the complete code for the  BankAccount class.
